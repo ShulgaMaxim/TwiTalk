8470a6bf76bd3695f8874e3fd254e98863fd4071
Shulga Max <mslulgaa@gmail.com>
2013-06-06 16:07:49 +0600

    Теперь есть возможность запоминать ID авторизированного пользователя

--- src/ru/usu/twitalk/Data.java
+++ src/ru/usu/twitalk/Data.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 public class Data {
 
 	public static ArrayList<String> FOLLOWERS = new ArrayList<String>();
-	public static String USER_NAME = "user_name";
+	public static String USER_NAME;
+	public static String USER_TEXT;
+	public static long ID_AUTH_USER;
     
 }
