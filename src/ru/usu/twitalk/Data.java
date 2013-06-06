1ac70bdfbf25d2e34635067bb3190670c8d3c697
Shulga Max <mslulgaa@gmail.com>
2013-06-06 11:22:07 +0600

    Класс будет хранить в себе данные

---
+++ src/ru/usu/twitalk/Data.java
@@ -0,0 +1,10 @@
+package ru.usu.twitalk;
+
+import java.util.ArrayList;
+
+public class Data {
+
+	public static ArrayList<String> FOLLOWERS = new ArrayList<String>();
+	public static String USER_NAME = "user_name";
+    
+}
