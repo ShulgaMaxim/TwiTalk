99a3df2fb1aba9d86a5f5704f7bd9f1b2bef87c7
Shulga Max <mslulgaa@gmail.com>
2013-06-08 23:00:30 +0600

    Изменена структура хранения данных, а именно. Вместо списка фолловеров теперь хэш(имя,идентификатор). В дальнейшем вся информация об авторизированном пользователе будет занесена в хэш.

--- src/ru/usu/twitalk/Data.java
+++ src/ru/usu/twitalk/Data.java
@@ -1,14 +1,18 @@
 package ru.usu.twitalk;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Data {
 
 	public static ArrayList<String> FOLLOWERS = new ArrayList<String>();
+	public static Map<String,Long> infAbFollowers = new HashMap<String,Long>();
 	public static String USER_NAME;
 	public static String USER_TEXT;
+	public static String USER_SCREEN_NAME;
 	public static long ID_AUTH_USER;
-	
-	//public static TreeMap <String, String[]> contactsWithMsgs = new TreeMap <String, String[]>();
+	public static Map<String, ArrayList<String>> contactsWithMsgs = new TreeMap<String, ArrayList<String>>();
 
 }
